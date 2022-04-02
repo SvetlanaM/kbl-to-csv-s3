5ff8dcc94db6ecb2abc299e27b51952793007eee
SvetlanaM <svetlana@kompilator.cz>
2022-04-02 10:46:23 +0200

    Add files

---
+++ README.md
@@ -0,0 +1,19 @@
+# KBL - Excel to CSV extractor
+
+Keboola extractor, that converts excel to CSV based on input mapping criteria.
+
+1. Set up the tables in the UI of input mapping in Keboola
+2. Set up the configuration json in the following way:
+
+```
+{
+  "parameters": {
+    "S3key": "XXXXXXX",
+    "S3secretKey":"XXXXXXXXX",
+    "bucketName":"XXXXXXXXXX"
+  }
+}
+```
+
+3. The component will then take all the input tables, transforms them into .xlsx
+   files and uploads to S3 accordingly to your json settings.
---
+++ code.py
@@ -0,0 +1,49 @@
+import ucsv as csv
+import os, glob
+import sys
+import xlsxwriter
+import tinys3
+import time
+import json
+from ctext import convert
+
+with open('./data/config.json') as jsonFile:
+    data = json.load(jsonFile)
+
+if data["parameters"]["#S3key"] == '' or data["parameters"]["#S3secretKey"] == '' or data["parameters"]["bucketName"] == '':
+    print (" === config json parameters empty ===")
+    sys.exit(1)
+
+if __name__ == '__main__':
+    listOfFiles = glob.glob("./data/in/tables/*.csv")
+    list_of_indexes = [[]]
+    sheet_names = []
+    for fileName in listOfFiles:
+        sheet_names.append(fileName[17:-4])
+    for index, fileInList in enumerate(listOfFiles):
+        with open(fileInList, 'rb') as f:
+            content = csv.reader(f)
+            for index_row, data_in_row in enumerate(content):
+                for index_col, data_in_cell in enumerate(data_in_row):
+                    if type(data_in_cell) == unicode and index_row != 0 and data_in_cell !="":
+                        list_of_indexes[index].append(index_col)
+                        list_of_indexes[index] = dict.fromkeys(list_of_indexes[index]).keys()
+            list_of_indexes.append([])
+            convert(list_of_indexes, sheet_names)
+
+    conn = tinys3.Connection(data["parameters"]["#S3key"],data["parameters"]["#S3secretKey"])
+    listOfFiles2 = glob.glob("./data/in/tables/*.xlsx")
+    for file in listOfFiles2:
+        temp = os.path.splitext(file[17:len(file)])
+        if data["parameters"]["fileName"] == "":
+            print(" === uploading " + 'CE_WK'+str(int(time.strftime("%V")))+temp[1] + " ===")
+            conn.upload('CE_WK'+str(int(time.strftime("%V")))+temp[1],open(file,'rb'),data["parameters"]["bucketName"])
+            print(" === " + 'CE_WK'+str(int(time.strftime("%V")))+temp[1] + " uploaded ===")
+        else:
+            print(" === " + data["parameters"]["fileName"] + ".xlsx" + " uploaded ===")
+            conn.upload(data["parameters"]["fileName"] + ".xlsx",open(file,'rb'),data["parameters"]["bucketName"])
+            print(" === " + data["parameters"]["fileName"] + ".xlsx" + " uploaded ===")
+            
+            
+        
+        
